9e04b37efecd9c4f84254643f8836de5cc0d6ef6
moelshenawy <mohamedgk5@gmail.com>
2023-03-02 17:06:21 +0200

    try gilory free light font

--- components/Home/ContactDetails/index.jsx
+++ components/Home/ContactDetails/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import styles from './index.module.scss'
+import imgs from "../../../assets/constants/imgs";
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
+import { FaShieldAlt } from 'react-icons/fa'
+import { MdLocationOn } from 'react-icons/md'
+import { consts } from 'react-elastic-carousel';
+import { Container, Typography, Rating, } from '@mui/material';
+import Link from 'next/link';
 
 const ContactDetails = () => {
   return (
-    <div>ContactDetails</div>
+    <section id='contact_details'>
+      <Container sx={{ maxWidth: '1239px' }} maxWidth={false}>
+        <div className="section_container">
+
+          <div className="talk">
+            <div className="title">
+
+            </div>
+          </div>
+
+
+          <div className="personal_manger"></div>
+
+
+          <div className="talk"></div>
+        </div>
+      </Container>
+
+    </section>
   )
 }
 
